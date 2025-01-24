64c393d91e15f35da70d321a1fda7db801d5f8b9
Scorpion <164779656+scorpion-00@users.noreply.github.com>
2025-01-25 04:19:05 +0530

    Update info_routes.py

--- app/api/info_routes.py
+++ app/api/info_routes.py
@@ -6,25 +6,17 @@ router = APIRouter()
 
 @router.get("/list_teams")
 def list_teams():
-    """
-    List all teams
-    """
-    teams = list_all_teams()
-    return teams
+    """List all teams"""
+    return list_all_teams()
 
 @router.get("/players/{team_id}")
 def list_players(team_id: int):
-    """
-    List all players for a team
-    """
-    players = list_all_players(team_id)
-    return players
+    """List all players for a team"""
+    return list_all_players(team_id)
 
 @router.get("/team_and_players")
 def list_teams_and_players():
-    """
-    List all teams and their players in it.
-    """
+    """List all teams and their players"""
     teams = list_all_teams()
     for team in teams:
         team["players"] = list_all_players(team["id"])
